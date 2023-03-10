55498562ec296fe16f397fc30d3bac7bd5c85205
daniyal1020 <107762034+daniyal1020@users.noreply.github.com>
2023-03-10 11:44:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+music1 = "";
+music2 = "";
+function preload(){
+    //music1 = loadSound("1music.mp3");
+    //music2 = loadSound("2music.mp3");
+}
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video, 0, 0, 400, 400);
+}
